92586f63afa679c8adef94765f2f0048d29284af
Krystian Wołoszun <krystianwoloszun@gmail.com>
2026-04-20 00:31:41 +0200

    style: better text

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -19,15 +19,24 @@ export default function Hero() {
 
       <div className="absolute inset-0 bg-black/20 -z-10" />
 
-      <div className={`w-full px-6 md:px-20 lg:w-1/5 text-left text-white z-10 ${bebas.className}`}>
+      {/* BLOK PO LEWEJ */}
+      <div
+        className={`relative z-10 text-white ${bebas.className} ml-12 md:ml-20 lg:ml-32 w-full max-w-3xl`}
+      >
 
-        <h1 className="text-6xl md:text-7xl tracking-widest uppercase">
-          Moonshine
-        </h1>
+        <div className="flex flex-col items-start text-left">
 
-        <p className="mt-4 text-sm md:text-base tracking-wide text-gray-200">
-          70% rock 40% unexpected twists
-        </p>
+          <img
+  src="/moonshine_logo_long.jpg"
+  alt="Moonshine"
+  className="w-full max-w-3xl md:max-w-4xl h-auto object-contain -translate-x-10 md:-translate-x-16 lg:-translate-x-50"
+/>
+
+<p className="w-full text-center text-base md:text-lg lg:text-2xl tracking-wide text-gray-200 -translate-x-10 md:-translate-x-16 lg:-translate-x-50">
+  70% rock 40% unexpected twists
+</p>
+
+        </div>
 
       </div>
 
