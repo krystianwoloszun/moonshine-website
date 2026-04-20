e998b2d8ae7fa2e1ca0b73619200fc6732e92988
Krystian Wołoszun <krystianwoloszun@gmail.com>
2026-04-20 16:48:16 +0200

    add background grain animation

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,28 +9,26 @@ export default function Hero() {
   return (
     <section className="relative h-screen w-full overflow-hidden">
 
-      {/* BACKGROUND */}
-      <div className="absolute inset-0 -z-10">
+      {/* BACKGROUND WRAPPER */}
+      <div className="absolute inset-0 -z-10 overflow-hidden">
         <img
           src="/banner_wall.JPG"
           alt="Hero background"
           className="w-full h-full object-cover"
         />
-      </div>
 
-      {/* OVERLAY */}
-      <div className="absolute inset-0 bg-black/30 -z-10" />
+        {/* DARK OVERLAY */}
+        <div className="absolute inset-0 bg-black/40" />
+
+        <div className="grain" />
+      </div>
 
       {/* LOGO BLOCK */}
       <div
         className={`
           absolute ${bebas.className}
-
-          /* <1200px → center */
           left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2
           text-center
-
-          /* ≥1200px → left */
           [@media(min-width:1200px)]:left-[5%]
           [@media(min-width:1200px)]:top-[45%]
           [@media(min-width:1200px)]:-translate-x-0
@@ -38,7 +36,6 @@ export default function Hero() {
           [@media(min-width:1200px)]:text-left
         `}
       >
-
         <img
           src="/PoziomeLogo.svg"
           alt="Moonshine"
@@ -50,9 +47,7 @@ export default function Hero() {
             [@media(min-width:1200px)]:mx-0
           "
         />
-
       </div>
-
     </section>
   );
 }
